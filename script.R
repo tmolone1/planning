9f2b1cc58ccb31640e6745205005dd606f55c2ff
Tim Moloney <tmoloney@trihydro.com>
2021-07-30 10:00:31 -0600

    made some revisions to the schedule

--- script.R
+++ script.R
@@ -1,15 +1,17 @@
 library(readxl)
 library(tidyverse)
 library(knitr)
-training<-read_excel("C:/Users/tmoloney/OneDrive - Trihydro/Fitness/202012_TrainingPlan.xlsx")
+library(lubridate)
+training<-read_excel("C:/Users/tmoloney/OneDrive - Trihydro/Fitness/202108_TrainingPlan.xlsx")
 today<-weekdays.Date(Sys.Date())
-format(Sys.Date(), '%m/%d/%Y')-days(2)
-t<-ceiling(difftime(Sys.Date(),as.Date("2020-12-07"),units="weeks"))
+#format(Sys.Date(), '%m/%d/%Y')-days(2)
+t<-ceiling(difftime(Sys.Date(),as.Date("2021-07-12"),units="weeks"))
 t<-as.character(t)
 today_train<-training[training$`Week:`==today,t]
 
 
 
-
+Sys.Date()+1
 
 rmarkdown::render("1.Rmd", output_file = "1.docx")
+rmarkdown::render("2.Rmd", output_file = "2.docx")
